383abd28b25e644ca82c62b13eae1172128c174d
kpingul <kirckpingul@gmail.com>
2015-07-16 23:29:01 -0700

    added a jquery plugin that allows the user to scroll up in the main page

--- foundationPlugin.js
+++ foundationPlugin.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
 
 	$(document).foundation();
+
 	  $.scrollUp({
         scrollName: 'scrollUp',      // Element ID
         scrollDistance: 300,         // Distance from top/bottom before showing element (px)
         scrollFrom: 'top',           // 'top' or 'bottom'
-        scrollSpeed: 300,            // Speed back to top (ms)
-        easingType: 'linear',        // Scroll to top easing (see http://easings.net/)
+        scrollSpeed: 600,            // Speed back to top (ms)
+        easingType: 'swing',        // Scroll to top easing (see http://easings.net/)
         animation: 'fade',           // Fade, slide, none
-        animationSpeed: 500,         // Animation speed (ms)
+        animationSpeed: 900,         // Animation speed (ms)
         scrollTrigger: false,        // Set a custom triggering element. Can be an HTML string or jQuery object
         scrollTarget: false,         // Set a custom target element for scrolling to. Can be element or number
         scrollText: '<i class="fa fa-chevron-up"></i>', // Text for element, can contain HTML
