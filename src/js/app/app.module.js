f152839b6a09301057825d795ada5d27afb48b4c
kpingul <kirckpingul@gmail.com>
2015-08-16 23:34:23 -0700

    update module dependencies with mm foundation since I accidently removed it

--- src/js/app/app.module.js
+++ src/js/app/app.module.js
@@ -8,12 +8,12 @@
 
 var myApp = angular.module('myApp', [
 
-	'angularSpinner', 
 	'ui.router', 
-	'angular-ui-view-spinner',
-	'mm.foundation',
 	'slickSlider',
 	'angular-ladda',
+	'angularSpinner', 
+	'angular-ui-view-spinner',
+	'mm.foundation',
 ]);
 
 
